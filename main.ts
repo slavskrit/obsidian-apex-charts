edd8d8084c92b2f43375d43efef6f5fc33bb289d
slavskrit <dpronin@yandex.ru>
2024-05-01 22:40:48 -0700

    updated?

--- main.ts
+++ main.ts
@@ -1,134 +1,115 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import ApexCharts from 'apexcharts'
+import { ApexOptions } from "apexcharts";
+import { Plugin, TFile, getAllTags } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+const TAG_TO_ENABLE = "#obsicharts";
+const TAG_TIMELINE = "#timeline";
 
-interface MyPluginSettings {
-	mySetting: string;
+interface TfileAndTags {
+	tfile: TFile,
+	tags: Array<string>
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+type CHART_TYPE = 'timeline' | 'addMORE';
+
+function buildTimelineOptions(filesWithTags: Array<TfileAndTags>): ApexOptions {
+	const ranges = filesWithTags
+		.filter((f) => f.tags.includes(TAG_TIMELINE))
+		.map((f) => {
+			const metadata = this.app.metadataCache.getFileCache(f.tfile);
+			return {
+				x: f.tfile.name,
+				y: [
+					new Date(metadata.frontmatter.start).getTime(),
+					new Date(metadata.frontmatter.end).getTime(),
+				],
+				meta: f.tfile.path,
+			}
+		});
+
+	return {
+		series: [
+			{
+				data: ranges
+			}
+		],
+		chart: {
+			height: 150,
+			type: 'rangeBar',
+			events: {
+				dataPointSelection: (_, __, config) => {
+					const path = ranges[config.dataPointIndex].meta;
+					const file = this.app.vault.getFileByPath(path);
+					this.app.workspace.activeLeaf.openFile(file);
+
+				},
+			}
+		},
+		plotOptions: {
+			bar: {
+				horizontal: true
+			}
+		},
+		xaxis: {
+			type: 'datetime'
+		}
+	};
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+function getChartOptions(chartType: CHART_TYPE): ApexOptions {
+	const allDocumentsToProcess = getAllTheDocumentsToWorkWith();
+	switch (chartType) {
+		case 'timeline': {
+			return buildTimelineOptions(allDocumentsToProcess);
+		}
+	}
+	return {};
+}
 
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+function getAllTheDocumentsToWorkWith(): TfileAndTags[] {
+	const cache = this.app.metadataCache;
+	const files = this.app.vault.getMarkdownFiles();
+	const filesWithTags: Array<TfileAndTags> = [];
+	files.forEach((file: TFile) => {
+		const fileCache = cache.getFileCache(file);
+		const tags = getAllTags(fileCache);
+		if (tags?.includes(TAG_TO_ENABLE)) {
+			filesWithTags.push(
+				{
+					tfile: file,
+					tags: tags,
 				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+			)
+		}
+	});
+	return filesWithTags;
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+// TODO: Handle exceptions.
+export default class ObsiChartsPlugin extends Plugin {
+	async onload() {
+		this.registerMarkdownCodeBlockProcessor("obsicharts", (source, el, ctx) => {
+			// console.log('register');
+			const chartType = source.split('\n')[0];
+			const options = getChartOptions(chartType);
+			// el.empty();
+			const a = el.createDiv();
+			a.textContent = "test";
+			const t = a.createDiv("TESToaHEAD:refs/heads/master")
+			//
+			// console.log(source, el, ctx);
+			// const a = ctx.addChild("div");
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			// console.log(a);
+			var chart = new ApexCharts(t, options);
+			chart.render();
+			// const chartContainer = el.createDiv();
+			// chartContainer.setAttribute("id", "chart")
+			// ctx.addChild(chartContainer);
+			// const chartType = source.split('\n')[0];
+			// const options = getChartOptions(chartType);
+			// var chart = new ApexCharts(document.querySelector("#chart"), options);
+			// chart.render();
+		});
 	}
 }
